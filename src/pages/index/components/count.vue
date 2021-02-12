c7b16e774f5bf0f47bcd0be6d5a9b725b11e65f1
ivanjo39191 <ivanjo39191@gmail.com>
2021-02-13 06:56:07 +0800

    chore: 添加 pages 拆分 vue 前後台 問題: 1. 後台使用 vue-element-admin 2. 前台另外拆分一個 vue 項目
    原因:
    1. 前後台分離
    
    調整項目:
    1. 更新套件
    2. vue.config.js 調整
    3. 添加  Vue 後台 admin
    添加 Vue 前台 index

---
+++ src/pages/index/components/count.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="content-box">
+    <h2>{{ msg }}</h2>
+    <hr>
+    <h3>{{$store.state.count.count}}</h3>
+    <h3>{{count}}</h3>
+    <div>
+      <button @click="$store.commit('add',10)">+</button>
+      <button @click="reduce">-</button>
+    </div>
+    <p>
+      <button @click="addAction">+</button>
+      <button @click="reduceAction">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import store from '../store/index'
+import {mapState, mapMutations, mapGetters, mapActions} from 'vuex';
+
+export default {
+  data () {
+    return {
+      msg: 'Hello Vuex'
+    }
+  },
+  computed:{
+    ...mapState(["count"]),
+    ...mapGetters(["count"])
+  },
+  methods:{
+    ...mapMutations([  
+        'add','reduce'
+    ]),
+    ...mapActions(['addAction','reduceAction'])
+  },
+  store
+}
+
+</script>
---
+++ vue.config.js
@@ -0,0 +1,135 @@
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+const port = process.env.port || process.env.npm_config_port || 9527 // dev port
+
+// vue.config.js
+var glob = require("glob");
+function getEntries(globPath) {
+  // 获取所有匹配文件的文件名数组
+  var files = glob.sync(globPath),
+    entries = {};
+
+  files.forEach(function (filepath) {
+    // 取倒数第二层(view下面的文件夹)做包名
+    var split = filepath.split("/");
+    var name = split[split.length - 2];
+
+    // 保存{'目录名': '目录路径'}
+    entries[name] = filepath;
+  });
+  return entries;
+}
+// 获取所有匹配src下目录的文件夹名字，其中文件夹里main.js为页面入口
+var pages = getEntries("src/**/main.js");
+console.log(pages); // 打印看看
+
+
+module.exports = {
+  // 其他选项
+  // pages 选项
+  pages: pages,
+  /**
+   * You will need to set publicPath if you plan to deploy your site under a sub path,
+   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
+   * then publicPath should be set to "/bar/".
+   * In most cases please use '/' !!!
+   * Detail: https://cli.vuejs.org/config/#publicpath
+   */
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    before: require('./mock/mock-server.js')
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    resolve: {
+      alias: {
+        '@': resolve('src/pages/admin')
+      }
+    }
+  },
+  chainWebpack(config) {
+    // it can improve the speed of the first screen, it is recommended to turn on preload
+    // config.plugin('preload').tap(() => [
+    //   {
+    //     rel: 'preload',
+    //     // to ignore runtime.js
+    //     // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
+    //     fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+    //     include: 'initial'
+    //   }
+    // ])
+
+    // when there are many pages, it will cause too many meaningless requests
+    config.plugins.delete('prefetch')
+
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/pages/admin/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/pages/admin/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+
+    config
+      .when(process.env.NODE_ENV !== 'development',
+        config => {
+          config
+            .plugin('ScriptExtHtmlWebpackPlugin')
+            .after('html')
+            .use('script-ext-html-webpack-plugin', [{
+              // `runtime` must same as runtimeChunk name. default is `runtime`
+              inline: /runtime\..*\.js$/
+            }])
+            .end()
+          config
+            .optimization.splitChunks({
+              chunks: 'all',
+              cacheGroups: {
+                libs: {
+                  name: 'chunk-libs',
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: 10,
+                  chunks: 'initial' // only package third parties that are initially dependent
+                },
+                elementUI: {
+                  name: 'chunk-elementUI', // split elementUI into a single package
+                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                },
+                commons: {
+                  name: 'chunk-commons',
+                  test: resolve('src/components'), // can customize your rules
+                  minChunks: 3, //  minimum common number
+                  priority: 5,
+                  reuseExistingChunk: true
+                }
+              }
+            })
+          // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
+          config.optimization.runtimeChunk('single')
+        }
+      )
+  }
+};
